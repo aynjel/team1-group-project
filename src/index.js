b316b6d2b27a50aab52befb0812231e837fb0cec
Serenity8888 <140626434+Serenity8888@users.noreply.github.com>
2024-02-24 11:35:12 +0800

    11th commit

--- src/index.js
+++ src/index.js
@@ -52,6 +52,7 @@ getTrendingMovies('week').then(response => {
 // Select the form and input field
 var searchForm = document.querySelector('.search-form');
 var searchQuery = document.querySelector('#searchQuery');
+var errorMessage = document.querySelector('.error-message');
 
 searchForm.addEventListener('submit', async function(event) {
   event.preventDefault(); // Prevent default form submission behavior
@@ -64,6 +65,12 @@ searchForm.addEventListener('submit', async function(event) {
 
     var dataResult = data.data.results;
     var movies = '';
+
+    if (dataResult.length === 0) {
+      // If no results found, display an error message
+      displayError('Search result not successful. Enter the correct movie name and');
+      return; // Exit the function early
+    }
 
     dataResult.forEach(movie => {
       var genreNames = [];
@@ -91,9 +98,27 @@ searchForm.addEventListener('submit', async function(event) {
   } catch (error) {
     console.error('Error:', error.message);
     // Handle errors (e.g., display error message to the user)
+    displayError('Search result not successful. Enter the correct movie name and try again.');
   }
 });
 
+function displayError(message) {
+  // Display error message on UI
+  var errorMessageElement = document.createElement('p');
+  errorMessageElement.textContent = message;
+  errorMessageElement.style.color = 'red';
+  
+  // Clear previous error messages if any
+  var existingErrorMessage = document.querySelector('.error-message');
+  if (existingErrorMessage) {
+    existingErrorMessage.remove();
+  }
+  
+  // Append new error message
+  var formContainer = document.querySelector('.form-control-container');
+  formContainer.appendChild(errorMessageElement);
+  errorMessageElement.classList.add('error-message');
+}
 
 // MODAL FUNCTION START
 const closeModal = document.querySelector('.modal-close-btn');
